2fbda5ce571b6c775bf708ea7ef877df2fccfa3b
Tomáš Duchoslav <130918563+GosutoDev@users.noreply.github.com>
2023-11-08 11:21:24 +0100

    Initial Commit

---
+++ newroll/newrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  newrollApp.swift
+//  newroll
+//
+//  Created by Tomáš Duchoslav on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct newrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
